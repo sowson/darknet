9f3b3e5670e8b87512743d1ce40c6c023a7b4745
Piotr Sowa <piotr@Pisowa.io>
2025-09-13 07:37:17 +0200

    New LibChess by :owl:

--- 3rdparty/libchess/libchess/position.hpp
+++ 3rdparty/libchess/libchess/position.hpp
@@ -210,15 +210,8 @@ class Position {
 
     [[nodiscard]] Move parse_move(const std::string &str) const {
         const auto moves = legal_moves();
-        const auto wksc = str == "e1g1" && piece_on(squares::E1) == Piece::King && turn() == Side::White;
-        const auto wqsc = str == "e1c1" && piece_on(squares::E1) == Piece::King && turn() == Side::White;
-        const auto bksc = str == "e8g8" && piece_on(squares::E8) == Piece::King && turn() == Side::Black;
-        const auto bqsc = str == "e8c8" && piece_on(squares::E8) == Piece::King && turn() == Side::Black;
-        const auto ksc = wksc | bksc;
-        const auto qsc = wqsc | bqsc;
         for (const auto &move : moves) {
-            if ((ksc && move.type() == MoveType::ksc) || (qsc && move.type() == MoveType::qsc) ||
-                static_cast<std::string>(move) == str) {
+            if (static_cast<std::string>(move) == str) {
                 return move;
             }
         }
